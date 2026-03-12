60bdd2ab3fb062fd458a5b09c40db8d37851ab67
erikraft <contact+github@erikraft.com>
2026-03-12 18:13:39 -0300

    Update

--- script.js
+++ script.js
@@ -383,6 +383,7 @@ function getCharacterScript(character) {
 // Exportar funções para uso global
 window.CharacterHub = {
     copyCharacter,
+    copyUnicode,
     shareCharacter,
     searchCharacters,
     filterByCategory,
